c9ea6e2176e76f63c00dc128e8aea367828edf26
iamarsalanofficial <iamarsalanofficial1@gmail.com>
2024-11-18 02:34:10 +0500

    Change All HTML Pages Into Laravel Blade Template.

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/services', function () {
+    return view('services');
+});
+Route::get('/blog', function () {
+    return view('blog');
+});
+Route::get('/blog-detail', function () {
+    return view('blog_detail');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/feature', function () {
+    return view('feature');
+});
+Route::get('/pricing-plan', function () {
+    return view('price');
+});
+Route::get('/team-members', function () {
+    return view('team');
 });
